e289742cf558a61ec408b1e31714ada193c6d679
M4RC0N3 <marcone@quickup.io>
2023-01-29 22:25:45 -0300

    hello

---
+++ hello.js
@@ -0,0 +1,16 @@
+let canvas = document.querySelector('canvas');
+
+let context = canvas.getContext('2d');
+context.fillStyle = 'pink'
+//context.fillRect(100, 100, 400, 400);
+
+// draw path
+context.lineWidth = '4'
+context.beginPath();
+
+context.rect(100, 100, 400 ,400);
+context.stroke()
+
+context.beginPath();
+context.arc(300, 300, 100, 0,Math.PI * 2);
+context.stroke()
